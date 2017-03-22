1b1fd1139861218f1ec63c61bcc2168d484a6b37
Kamila Kuryło <kamila888k@gmail.com>
2017-03-22 13:28:24 +0100

    Dodanie @Transactional do zapomnianego endpointa

--- src/main/java/pl/kokokoko/controller/RelationshipOwnerCarEndpoint.java
+++ src/main/java/pl/kokokoko/controller/RelationshipOwnerCarEndpoint.java
@@ -2,6 +2,7 @@ package pl.kokokoko.controller;
 
 import io.spring.guides.gs_producing_web_service.AssignCarToOwnerRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -9,6 +10,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import pl.kokokoko.domain.RelationshipOwnerCar;
 
 @Endpoint
+@Transactional
 public class RelationshipOwnerCarEndpoint {
 
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
